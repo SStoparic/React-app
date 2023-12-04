90ea8eac00ab92e2ae6a8d26a8a44a991e82b10f
SStoparic <sinisa.stoparic99@gmail.com>
2023-12-04 19:46:42 +0100

    Uradjen footer i njegov css po istom prinicipu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import NavBar from './components/NavBar';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
-   
+   <div>
     <BrowserRouter>
         <NavBar></NavBar>
      </BrowserRouter>
 
+    <Footer></Footer>
+    </div>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+      <div>
+        <footer className="footer-container"> 
+            <div className="footer-text" >
+                © 2023 Copyright: BELGRADE GYMS
+            </div> 
+            <div className="footer-text" >
+                Email: belgrade.gyms@gmail.com
+            </div> 
+            <div className="footer-text" >
+                Contact: +38165838747
+            </div> 
+        </footer> 
+      </div>
+    );
+  }
+
+  export default Footer;
